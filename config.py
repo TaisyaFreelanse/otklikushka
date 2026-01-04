807c7279024aa922060f9980cea2b2d932d1250d
TaisyaFreelanse <smilikgirl1@gmail.com>
2026-01-04 21:38:11 +0200

    Add default Telegram user ID 1797952290 to configuration

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DEFAULT_CATEGORIES = os.getenv("DEFAULT_CATEGORIES", "").split(",") if os.getenv
 # Security
 ALLOWED_USER_IDS = [
     int(uid.strip()) 
-    for uid in os.getenv("ALLOWED_USER_IDS", "").split(",") 
+    for uid in os.getenv("ALLOWED_USER_IDS", "1797952290").split(",")  # Default user ID
     if uid.strip()
 ]
 
